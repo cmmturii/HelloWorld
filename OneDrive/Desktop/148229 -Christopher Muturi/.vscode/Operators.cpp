723716dbc23234a1e9c55b32160a0d3e601a3e82
New Author Name <new.email@example.com>
2025-11-17 18:52:28 +0300

    New files

--- OneDrive/Desktop/148229 -Christopher Muturi/.vscode/Operators.cpp
+++ OneDrive/Desktop/148229 -Christopher Muturi/.vscode/Operators.cpp
@@ -27,12 +27,32 @@ int main() {
     cout << "DECREMENT: " << num2-- << " = " << num2 << endl; // Post-decrement
     
     // Reset for combined operations
-    num1 = 16;
+    num1 = 16; num2 =4;
     
     cout << "COMBINED INCREMENT AND ASSIGNMENT: num1 += 2; " << (num1 += 2) << endl;
     cout << "COMBINED DECREMENT AND ASSIGNMENT: num1 -= 2; " << (num1 -= 2) << endl;
     cout << "COMBINED MULTIPLY AND ASSIGN: num1 *= 2; " << (num1 *= 2) << endl;
     cout << "COMBINED DIVIDE AND ASSIGN: num1 /= 2; " << (num1 /= 2) << endl;
 
+    // COMPARISON/RELATIONAL OPERATORS
+    cout << "3>2" << "="<<(3>2) << endl;        // Greater than
+    cout << "3<2" << "="<<(3<2) << endl;       // Less than
+    cout << "4>=3" << "="<<(4>=3) << endl;;       // Greater than or equal to
+    cout << "4<3" << "="<<(4<=3) << endl;       // Less than or equal to
+    cout << "3==3" << "="<<(3==3) << endl;        // Equal to
+    cout << "3!=3" << "="<<(3!=3) << endl;    //  Not equal to
+    cout << "!(4>=3)" << "="<<!(4>=3) << endl;     // Not operator
+
+  // BOOLEAN OPERATORS (AND &&, OR ||, NOT !)
+    cout << "True && True = " << ((3 > 2) && (4 >= 2)) << endl;
+    cout << "True && False = " << ((3 > 2) && (4 <= 2)) << endl;
+    cout << "False && True = " << ((3 < 2) && (4 >= 2)) << endl;
+    cout << "False && False = " << ((3 < 2) && (4 >= 2)) << endl;
+    
+    // Boolean OR
+    cout << "True || True = " << ((3 > 2) || (4 >= 2)) << endl;
+    cout << "True || True = " << ((3 > 2) || (4 <= 2)) << endl;
+    cout << "True || True = " << ((3 > 2) || (4 >= 2)) << endl;
+    cout << "True || True = " << ((3> 2) || (4 >= 2)) << endl;
     return 0;
 }
---
+++ OneDrive/Desktop/148229 -Christopher Muturi/.vscode/text.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+ int main()
+ {
+    int num =5;
+    cout<<++num<<endl;
+     return 0;
+}
